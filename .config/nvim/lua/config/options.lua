a47e0c6826cae524115561b93551356f7937e66a
zxbuzzy <zxbuzzy@gmail.com>
2025-07-20 21:22:18 +0300

    add some options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -24,4 +24,20 @@ vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 5
+vim.opt.scrolloff = 10
+
+-- Show line under cursor
+vim.opt.cursorline = true
+
+-- Enable mouse mode (just in case)
+vim.opt.mouse = "a"
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Case insensitive search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn
+vim.opt.signcolumn = "yes"
